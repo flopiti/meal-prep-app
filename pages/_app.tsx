7410d0b0e07e06552428bb054295d428ab91f95e
flopiti <npieraut@live.fr>
2023-03-06 19:38:45 -0500

    BaseLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import BaseLayout from '@/components/BaseLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <UserProvider>
-    <Component {...pageProps} />
+    <BaseLayout>
+      <Component {...pageProps} />
+    </BaseLayout>
     </UserProvider>
 }
