9d1e36321b6fd62d83125a3b817b1bfebd578960
flopiti <npieraut@live.fr>
2023-03-06 20:08:14 -0500

    if there is Show meal

--- components/Meal.tsx
+++ components/Meal.tsx
@@ -3,7 +3,18 @@ import styles from '../styles/Home.module.css';
 const Meal = ({ meal } :any) => {
     return (
         <div className={styles.meal}>
-            {meal}
+            <div>{meal}</div>
+            {
+                meal ? (
+                    <div className={styles.mealChoice}>
+                        {meal}
+                    </div>
+                ) : (
+                    <div className={styles.mealChoice}>
+                        No meal here yet
+                    </div>
+                )
+            }
         </div>
     );
 };
