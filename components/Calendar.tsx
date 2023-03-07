5019a1cfbce82fbb3d49ae0b9621b474c556e058
flopiti <npieraut@live.fr>
2023-03-07 12:35:16 -0500

    key

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import styles from '@/styles/Home.module.css'
 import Day from '@/components/Day'
-import { useScheduledMeals } from '@/hooks/useScheduledMeals';
 
 const Calendar = ({scheduledMeals, scheduleMeal}:any) => {
   let datesToCover=['2023-03-08', '2023-03-09', '2023-03-10', '2023-03-11' ];
@@ -10,8 +9,8 @@ const Calendar = ({scheduledMeals, scheduleMeal}:any) => {
   return (
           <div className={styles.calendar}>
             {
-                datesToCover.map((day) => {
-                    return <Day scheduleMeal={scheduleMeal} day={day} meals={scheduledMeals.filter((meal:any) => meal.date === day)}/>
+                datesToCover.map((day, index) => {
+                    return <Day key={index} scheduleMeal={scheduleMeal} day={day} meals={scheduledMeals.filter((meal:any) => meal.date === day) }/>
                 })
             }
           </div>          
