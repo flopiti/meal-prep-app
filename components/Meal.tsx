7f40262601885376cdd31a9c74a02f10e840b103
flopiti <npieraut@live.fr>
2023-03-06 20:00:15 -0500

    prettier boxes

--- components/Meal.tsx
+++ components/Meal.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/Home.module.css';
+
 const Meal = ({ meal } :any) => {
     return (
-        <div>
+        <div className={styles.meal}>
             {meal}
         </div>
     );
