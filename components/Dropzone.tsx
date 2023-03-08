705d573c68c74e954bfd9cb4b1451b257ddd53e4
flopiti <npieraut@live.fr>
2023-03-08 16:24:54 -0500

    dropzone

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -4,8 +4,9 @@ import styles from '../styles/Home.module.css';
 export const DropZone = (props:any) => {
     const handleDrop = (event:any) => {
       event.preventDefault();
-      const data = event.dataTransfer.getData('text/plain');
-      props.onDrop(data);
+      const data = event.dataTransfer.getData('1');
+      const data2 = event.dataTransfer.getData('2');
+      props.onDrop(data, data2);
     };
   
     const handleDragOver = (event:any) => {
