9e4273951cce43721c749d5b01646c19d228737d
flopiti <npieraut@live.fr>
2023-03-10 01:04:38 -0500

    edit

--- components/Meal.tsx
+++ components/Meal.tsx
@@ -12,7 +12,7 @@ export type Meal = {
 
 const ScheduledMeal = ({ meal, mealType , scheduleMeal, day, removeMeal} :any) => {
     const { t } = useTranslation('common')
-    const { deleteScheduledMeal } = useScheduledMeals();
+    const { deleteScheduledMeal, putScheduledMeal } = useScheduledMeals();
     const deleteMeal = () => {
         if(!meal) return;
         deleteScheduledMeal(meal.id).then(() => {
@@ -26,7 +26,28 @@ const ScheduledMeal = ({ meal, mealType , scheduleMeal, day, removeMeal} :any) =
     };
 
     const { postScheduledMeal } = useScheduledMeals();
+
     const handleDrop = (data:any, iconUrl:any) => {
+        if(meal){
+            putScheduledMeal(meal.id, data, day, mealType, iconUrl, meal.mealName, meal.iconUrl).then((res:any) => {
+                console.log('hello')
+                removeMeal({
+                    mealName: meal.mealName,
+                    date: meal.date,
+                    mealType: meal.mealType,
+                    id: meal.id
+                });
+                scheduleMeal({
+                    mealName: res.mealName,
+                    date: res.date,
+                    mealType: res.mealType,
+                    id: res.id,
+                    iconUrl: res.iconUrl,
+                    meal2Name: res.meal2Name,
+                    icon2Url: res.icon2Url
+                });
+            });
+        }
         postScheduledMeal(data, day, mealType, iconUrl).then((res:any) => {
             scheduleMeal({
                 mealName: res.mealName,
@@ -50,6 +71,35 @@ const ScheduledMeal = ({ meal, mealType , scheduleMeal, day, removeMeal} :any) =
             <motion.div 
             >
             {
+                meal?.mealName && meal?.meal2Name ? (
+                    <DropZone onDrop={handleDrop}>
+                    <motion.div 
+                    className={styles.twoMealChoice}
+                    animate={{ backgroundColor: "#28afb0", scale: [0.25, 1] }}
+                    transition={{ duration: 2, type : "spring", stiffness: 200}}
+                    >
+                        <motion.div
+                            className={styles.mealIcon}
+                            initial={{ y: 150 }}
+                            animate={{ y: [150,5], rotate: [0, 360]                          
+                            }
+                        }   transition={{ duration: 1, stiffness: 100}}
+                        >
+                            {meal.iconUrl ?  <Image src={meal.iconUrl} alt="food" width={84} height={84} /> : <span></span>}
+                        </motion.div>
+                        <motion.div
+                            className={styles.mealIcon}
+                            initial={{ y: 150 }}
+                            animate={{ y: [150,5], rotate: [0, 360]                          
+                            }
+                        }   transition={{ duration: 1, stiffness: 100}}
+                        >
+                            {meal.icon2Url ?  <Image src={meal.icon2Url} alt="food" width={84} height={84} /> : <span></span>}
+                        </motion.div>
+                    </motion.div>
+                </DropZone>
+                ) :
+
                 meal ? (                        
                 <DropZone onDrop={handleDrop}>
                     <motion.div 
