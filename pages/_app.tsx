0645e2bd9403c53b5c6af057df690cb0720290b5
flopiti <npieraut@live.fr>
2023-03-09 23:56:39 -0500

    wrap it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import BaseLayout from '@/components/BaseLayout';
+import { appWithTranslation } from 'next-i18next'
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <UserProvider>
     <BaseLayout>
       <Component {...pageProps} />
     </BaseLayout>
     </UserProvider>
 }
+
+export default appWithTranslation(App);
