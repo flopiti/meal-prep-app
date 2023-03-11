beddc4e628dbe87abbba67a30516e72935c9169b
flopiti <npieraut@live.fr>
2023-03-11 16:10:23 -0500

    crossOrigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&family=Red+Hat+Display:wght@300&display=swap" rel="stylesheet"/>
       </Head>
       <body>
