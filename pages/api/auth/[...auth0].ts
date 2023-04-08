e1f756004afee3f951e0938acbc1977dc0588e52
flopiti <npieraut@live.fr>
2023-04-07 22:02:23 -0400

    returnTo

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -12,7 +12,8 @@ export default handleAuth(
                         audience: serverRuntimeConfig.auth0Audience,
                         scope : 'offline_access openid profile email',
                         redirect_uri: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI, // Add this line
-                    }
+                    },
+                    returnTo: process.env.NEXT_PUBLIC_AUTH0_RETURN_TO, // Add this line
                 })
             }
             catch(error:any){
