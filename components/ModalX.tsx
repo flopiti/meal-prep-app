8092678e5d4d30f120e8355347a01e1da8e4e5cb
flopiti <npieraut@live.fr>
2023-04-11 14:17:30 -0400

    modal comp

---
+++ components/ModalX.tsx
@@ -0,0 +1,27 @@
+import React, { EventHandler } from 'react';
+import styles from '../styles/Modal.module.css';
+
+const ModalX = ({ open , setOpen, children}:any) => {
+
+    const onClose = () => {
+        window.dialog.close()
+        setOpen(false)
+    };
+
+    
+  return (
+    <>
+      {open && <div className={styles.backdrop}></div>}
+      <dialog id="dialog" className={styles.dialog} open={open}>
+        {
+            children
+        }
+        <button onClick={onClose} aria-label="close" className={styles.x}>
+          ❌
+        </button>
+      </dialog>
+    </>
+  );
+};
+
+export default ModalX;
