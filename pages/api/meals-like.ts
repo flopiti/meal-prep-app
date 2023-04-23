26631322de0ca145fb0e3fef201a15e43dd3fb36
flopiti <npieraut@live.fr>
2023-04-23 15:12:06 -0400

    remove route

--- pages/api/meals-like.ts
+++ pages/api/meals-like.ts
@@ -17,39 +17,10 @@ const handleGetMethod = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(200).json(response?.data);
 }
 
-const handlePostMethod = async (req: NextApiRequest, res: NextApiResponse) => { 
-    const {accessToken} = await getAccessToken(req, res)
-    const response = await axios.post(`${process.env.BACKEND_URL}/meal-likes/${req.body.id}`, req.body, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-            'Authorization': `Bearer ${accessToken}`
-        }
-    })
-    return res.status(200).json(response?.data);
-}
-
-const handleDeleteMethod = async (req: NextApiRequest, res: NextApiResponse) => {
-    const {accessToken} = await getAccessToken(req, res)
-    const response = await axios.delete(`${process.env.BACKEND_URL}/meal-likes/${req.body.id}`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-        }
-    })
-    return res.status(200).json(response?.data);
-}
-
 const handler = (req: NextApiRequest, res: NextApiResponse<any[]>) => {
     switch(req.method) {
         case 'GET':
             return handleGetMethod(req, res);
-        case 'POST':
-            return handlePostMethod(req, res);
-        case 'DELETE':
-            return handleDeleteMethod(req, res);
         default:
             return res.status(405).end()
     }
