cd07a29a5644aa2db1ad3eeebd2498bdd82fb547
flopiti <npieraut@live.fr>
2023-04-28 01:19:48 -0400

    ingredient List

--- components/IngredientList.tsx
+++ components/IngredientList.tsx
@@ -1,4 +1,14 @@
-const IngredientList = ({ ingredients }:any) => {
+import { useState } from "react";
+import ModalX from "./ModalX";
+import AddIngredientForm from "./AddIngredientForm";
+
+const IngredientList = ({ ingredients, addIngredient }:any) => {
+    const[isOpen, setIsOpen] = useState<boolean>(false);
+
+    const showModal = () => {
+        setIsOpen(true);
+      };
+      
     return (
         <div >
             <ul className="ingredient-list">
@@ -6,6 +16,10 @@ const IngredientList = ({ ingredients }:any) => {
                     <li key={ingredient.id}>{ingredient.name}</li>
                 ))}
             </ul>
+            <button  onClick={showModal}>+</button>
+            <ModalX open={isOpen} setOpen={setIsOpen}> 
+            <AddIngredientForm closeForm={setIsOpen} addIngredient={addIngredient} />
+          </ModalX>
         </div>
         );
     };
