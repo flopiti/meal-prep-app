ca61b8db2ab7756a334f4b192ddc5136df8e0409
flopiti <npieraut@live.fr>
2023-04-30 14:54:40 -0400

    api call

--- pages/api/meals/[mealId].ts
+++ pages/api/meals/[mealId].ts
@@ -15,10 +15,25 @@ const handleGetMethod = async (req: NextApiRequest, res: NextApiResponse) => {
 return res.status(200).json(response?.data);
 };
 
+const handleDeleteMethod = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {accessToken} = await getAccessToken(req, res)
+  const response = await axios.delete(`${process.env.BACKEND_URL}/meals/${req.query.mealId}`, {
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        'Authorization': `Bearer ${accessToken}`
+    }
+})
+return res.status(200).json(response?.data);
+};
+
 const handler = (req: NextApiRequest, res: NextApiResponse<any[]>) => {
   switch (req.method) {
     case 'GET':
       return handleGetMethod(req, res);
+    case 'DELETE':
+      return handleDeleteMethod(req, res);
     default:
       return res.status(405).end();
   }
