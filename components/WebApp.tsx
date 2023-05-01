4d55fcf5f8455426a382e726f7af9279c51fa5d1
flopiti <npieraut@live.fr>
2023-05-01 00:24:00 -0400

    webapp

---
+++ components/WebApp.tsx
@@ -0,0 +1,38 @@
+import Calendar from "./Calendar";
+import IngredientList from "./IngredientList";
+import LikedMealsList from "./LikedMealsList";
+import { Meals } from "./Meals";
+import styles from "../styles/WebApp.module.css";
+
+interface WebAppProps {
+    scheduledMeals: any;
+    scheduleMeal: any;
+    removeMeal: any;
+    addMealToScheduledMeal: any;
+    likedMeals: any;
+    meals: any;
+    likeMeal: any;
+    unlikeMeal: any;
+    setLikedMeals: any;
+    addMeal: any;
+    removeMealFromList: any;
+    ingredients: any;
+    addIngredient: any;
+    deleteIngredient: any;
+}
+
+const WebApp = ({scheduleMeal, scheduledMeals, removeMeal, addMealToScheduledMeal, likedMeals, likeMeal, meals, addIngredient, unlikeMeal, setLikedMeals, addMeal, removeMealFromList, deleteIngredient, ingredients}:WebAppProps) => (
+    <div className={styles.flexMain}>
+        <div style={{ width: '80%', display: 'inline-block' }}>
+            <a href="/api/auth/logout">Logout</a>
+            <Calendar scheduledMeals={scheduledMeals} scheduleMeal={scheduleMeal} removeMeal={removeMeal} addMealToScheduledMeal={addMealToScheduledMeal} />
+            <LikedMealsList meals={likedMeals} />
+        </div>
+        <div style={{ width: '20%', display: 'inline-block', height: '96vh'}}>
+            <Meals likedMeals={likedMeals} meals={meals} likeMeal={likeMeal} unlikeMeal={unlikeMeal} setLikedMeals={setLikedMeals} addMeal={addMeal} removeMealFromList={removeMealFromList} />
+            <IngredientList ingredients={ingredients} addIngredient={addIngredient} removeIngredient={deleteIngredient} />
+        </div>
+    </div>
+);
+
+export default WebApp;
