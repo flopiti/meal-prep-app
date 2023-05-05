4cace8716afc6b5474be93825d7e416fbedc5712
Nathan <npieraut@live.fr>
2023-05-05 17:55:12 -0400

    content

--- components/Xbutton.tsx
+++ components/Xbutton.tsx
@@ -1,1 +1,13 @@
-import styles from './Xbutton.module.css';
+import styles from '../styles/XButton.module.css';
+
+export interface XButtonProps {
+    children: React.ReactNode;
+    onClick: () => void;
+}
+
+const XButton = (props: XButtonProps) => {
+    const { children, ...rest } = props;
+    return <button className={styles.button}{...rest}>{children}</button>;
+}
+
+export default XButton;
