5c6e8b8975e098791e560750fca69edb504ebb26
Nathan <npieraut@live.fr>
2023-05-07 16:54:44 -0400

    move the function down

--- components/_layouts/WebApp.tsx
+++ components/_layouts/WebApp.tsx
@@ -19,13 +19,14 @@ interface WebAppProps {
     ingredients: any;
     addIngredient: any;
     deleteIngredient: any;
+    changeMeal: any;
 }
 
-const WebApp = ({scheduleMeal, scheduledMeals, removeMeal, addMealToScheduledMeal, likedMeals, likeMeal, meals, addIngredient, unlikeMeal, setLikedMeals, addMeal, removeMealFromList, deleteIngredient, ingredients}:WebAppProps) => (
+const WebApp = ({scheduleMeal, scheduledMeals, removeMeal,changeMeal, addMealToScheduledMeal, likedMeals, likeMeal, meals, addIngredient, unlikeMeal, setLikedMeals, addMeal, removeMealFromList, deleteIngredient, ingredients}:WebAppProps) => (
     <div className={styles.flexMain}>
         <div style={{ width: '80%', display: 'inline-block' }}>
             <a href="/api/auth/logout">Logout</a>
-            <Calendar scheduledMeals={scheduledMeals} scheduleMeal={scheduleMeal} removeMeal={removeMeal} addMealToScheduledMeal={addMealToScheduledMeal} />
+            <Calendar scheduledMeals={scheduledMeals} scheduleMeal={scheduleMeal} removeMeal={removeMeal} changeMeal={changeMeal} addMealToScheduledMeal={addMealToScheduledMeal} />
             <LikedMealsList meals={likedMeals} />
         </div>
         <div style={{ width: '20%', display: 'inline-block', height: '96vh'}}>
