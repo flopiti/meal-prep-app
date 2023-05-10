2aa2f0b65f934834c6cfadd918699b49c34b96f8
Nathan <npieraut@live.fr>
2023-05-10 15:53:43 -0400

    api put meal

--- pages/api/meals/[mealId].ts
+++ pages/api/meals/[mealId].ts
@@ -15,6 +15,19 @@ const handleGetMethod = async (req: NextApiRequest, res: NextApiResponse) => {
 return res.status(200).json(response?.data);
 };
 
+const handlePutMethod = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {accessToken} = await getAccessToken(req, res)
+  const response = await axios.put(`${process.env.BACKEND_URL}/meals/${req.query.mealId}`,req.body, {
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        'Authorization': `Bearer ${accessToken}`
+    }
+})
+return res.status(200).json(response?.data);
+};  
+
 const handleDeleteMethod = async (req: NextApiRequest, res: NextApiResponse) => {
   const {accessToken} = await getAccessToken(req, res)
   const response = await axios.delete(`${process.env.BACKEND_URL}/meals/${req.query.mealId}`, {
@@ -32,6 +45,8 @@ const handler = (req: NextApiRequest, res: NextApiResponse<any[]>) => {
   switch (req.method) {
     case 'GET':
       return handleGetMethod(req, res);
+    case 'PUT':
+      return handlePutMethod(req, res);
     case 'DELETE':
       return handleDeleteMethod(req, res);
     default:
