a23d694e3e0e420148de2ce5ef9462b2bc173968
Nathan <npieraut@live.fr>
2023-05-10 15:52:44 -0400

    meal list admin

--- components/Admin/MealListAdmin/MealListAdmin.tsx
+++ components/Admin/MealListAdmin/MealListAdmin.tsx
@@ -1,15 +1,13 @@
 
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { useMeals } from "@/hooks/useMeals";
 import Image from 'next/image'
 import ModalX from "@/components/ModalX";
-import AddMealForm from "@/components/Modals/MealForm";
 import EditMealForm from "@/components/Modals/EditMealForm";
 
 const MealListAdmin = () => {
 
-    const { getMeals, getMealsLike, likeMeal, unlikeMeal , deleteMeal, editMeal} = useMeals();
+    const { getMeals, deleteMeal, editMeal} = useMeals();
     const [isOpen, setIsOpen] = useState<boolean>(false);
   
     const showModal = () => {
@@ -19,10 +17,20 @@ const MealListAdmin = () => {
     const [meals, setMeals] = useState<any[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+    const [chosenMeal, setChosenMeal] = useState<any>(null);
+
+    const editMealState = async (id:string, meal:any) => {
+        editMeal(id, meal).then((editedMeal:any) => {
+            setMeals(meals.map((m) => m.id === id ? editedMeal : m))
+            setIsOpen(false)
+        }).catch((err:any) => {
+            console.log(err)
+        }
+        )
+    }
     
     useEffect(() => {
         getMeals().then((data:any) => {
-            console.log(data)
             setMeals(data)
             setLoading(false)
         }).catch((err:any) => {
@@ -37,7 +45,7 @@ const MealListAdmin = () => {
         {loading && <span>loading...</span>}
         {error && <p>{error}</p>}
         <ul>
-            {meals.map((meal) => (
+            {meals?.map((meal) => (
             <li key={meal.id}>
                 <h3>{meal.mealName}</h3>
                 <p>{meal.description}</p>
@@ -46,9 +54,12 @@ const MealListAdmin = () => {
                 {
                     meal.iconUrl ?  <Image src={meal.iconUrl} alt={""} width={100} height={100}/> : <span></span>
                 }
-                <button onClick={() => showModal()}>Edit</button>
+                <button onClick={() => {
+                    setChosenMeal(meal)
+                    showModal()
+                }}>Edit</button>
                 <ModalX open={isOpen} setOpen={setIsOpen}> 
-                    <EditMealForm closeForm={()=>setIsOpen} meal={meal} editMeal={editMeal} />
+                    <EditMealForm closeForm={()=>setIsOpen} meal={chosenMeal} editMeal={editMealState} />
                 </ModalX>
             </li>
             ))}
