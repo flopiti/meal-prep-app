0a20633af9b822e97485359c64ac608d0f502304
Nathan <npieraut@live.fr>
2023-05-09 23:21:01 -0400

    admi page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,7 +1,10 @@
+import MealListAdmin from "@/components/Admin/MealListAdmin/MealListAdmin";
+
 export default function Admin() {
     return (
         <div>
         <h1>Admin</h1>
+        <MealListAdmin />
         </div>
     );
     }
