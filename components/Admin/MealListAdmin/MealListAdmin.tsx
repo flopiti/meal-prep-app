9b40ae00cc3e4aa519d2f921e79464e3bc50e63c
Nathan <npieraut@live.fr>
2023-05-11 12:23:03 -0400

    modal fix

--- components/Admin/MealListAdmin/MealListAdmin.tsx
+++ components/Admin/MealListAdmin/MealListAdmin.tsx
@@ -6,14 +6,20 @@ import ModalX from "@/components/ModalX";
 import EditMealForm from "@/components/Modals/EditMealForm";
 import MealCard from "@/components/MealCard/MealCard";
 import styles from "./MealListAdmin.module.css";
+import AddMealForm from "@/components/Modals/MealForm";
 
 const MealListAdmin = () => {
 
-    const { getMeals, deleteMeal, editMeal} = useMeals();
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-  
-    const showModal = () => {
-      setIsOpen(true);
+    const { getMeals, deleteMeal, editMeal, createMeal} = useMeals();
+    
+    const [isEditModalOpen, setIsEditModalOpen] = useState<boolean>(false);
+    const showEditModal = () => {
+        setIsEditModalOpen(true);
+    };
+
+    const [isAddModalOpen, setIsAddModalOpen] = useState<boolean>(false);
+    const showAddModal = () => {
+        setIsAddModalOpen(true);
     };
 
     const [meals, setMeals] = useState<any[]>([]);
@@ -21,21 +27,31 @@ const MealListAdmin = () => {
     const [error, setError] = useState<string | null>(null);
     const [chosenMeal, setChosenMeal] = useState<any>(null);
 
-    const editMealState = async (id:string, meal:any) => {
-        editMeal(id, meal).then((editedMeal:any) => {
-            setMeals(meals.map((m) => m.id === id ? editedMeal : m))
-            setIsOpen(false)
+
+
+    const addMeal = async (meal:any) => {
+        createMeal(meal).then((newMeal:any) => {
+            setMeals([...meals, meal])
+            setIsAddModalOpen(false)
         }).catch((err:any) => {
             console.log(err)
         }
         )
     }
 
-    // write the removemeal function call it deleteMeal and change the state like the editMealState
+    const editMealState = async (id:string, meal:any) => {
+        editMeal(id, meal).then((editedMeal:any) => {
+            setMeals(meals.map((m) => m.id === id ? editedMeal : m))
+            setIsEditModalOpen(false)
+        }).catch((err:any) => {
+            console.log(err)
+        }
+        )
+    }
+
     const deleteMealState = async (id:string) => {
-        deleteMeal(id).then((deletedMeal:any) => {
+        deleteMeal(id).then(() => {
             setMeals(meals.filter((m) => m.id !== id))
-            setIsOpen(false)
         }).catch((err:any) => {
             console.log(err)
         }
@@ -63,13 +79,17 @@ const MealListAdmin = () => {
             {
             meals.map((meal:any)=>(
                 <MealCard 
-                isOpen={isOpen}
-                setIsOpen={setIsOpen}
+                isOpen={isEditModalOpen}
+                setIsOpen={setIsEditModalOpen}
                 chosenMeal={chosenMeal}
-                meal={meal} deleteMeal={deleteMealState} editMealState={editMealState} showModal={showModal} setChosenMeal={setChosenMeal}/>
+                meal={meal} deleteMeal={deleteMealState} editMealState={editMealState} showModal={showEditModal} setChosenMeal={setChosenMeal}/>
             ))
             }   
         </div>
+        <button onClick={showAddModal}>+</button>
+        <ModalX open={isAddModalOpen} setOpen={setIsAddModalOpen}> 
+            <AddMealForm closeForm={()=>setIsAddModalOpen} addMeal={addMeal} />
+        </ModalX>
         </div>
     );
     };
