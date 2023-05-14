995d5a50843751be387a18b07bb2d196a2c2f6f7
Nathan <npieraut@live.fr>
2023-05-14 12:54:12 -0400

    unlike in the meal List

--- components/LikedMealsList.tsx
+++ components/LikedMealsList.tsx
@@ -1,6 +1,7 @@
 import styles from '@/styles/Home.module.css'
 import Image from 'next/image'
 import React, { useState } from "react";
+import LikeAnimation from './LikeAnimation';
 
 export type Meal = {
     id: number,
@@ -9,7 +10,7 @@ export type Meal = {
     iconUrl: string,
   }
 
-const LikedMealsList = ({meals}:any) => {
+const LikedMealsList = ({meals, unlikeMeal, setLikedMeals}:any) => {
 
     const [isDragging, setIsDragging] = useState(false);
 
@@ -18,6 +19,12 @@ const LikedMealsList = ({meals}:any) => {
         event.dataTransfer.setData('1', meals[index].id);
       };
 
+    const unlikeAndRemoveMeal = (id:number) => {
+        unlikeMeal(id).then(() => {
+            setLikedMeals(meals.filter((likedMeal: { id: number; }) => likedMeal.id !== id));
+        }
+        )
+    }
     return (
         <div className={styles.mealList}>
             {
@@ -28,6 +35,9 @@ const LikedMealsList = ({meals}:any) => {
                     draggable
                     onDragStart={()=>handleDragStart(event, index)}
                     className={styles.mealItem}>
+                        <LikeAnimation 
+                            liked={true} 
+                            onLikeChange={()=>unlikeAndRemoveMeal(meal.id)}/>
                         <span>{meal.mealName}</span>
                         {
                             meal.iconUrl ?  <Image className={styles.mealIconSmall}  src={meal.iconUrl} alt="food" width={25} height={25} /> : <span></span>
