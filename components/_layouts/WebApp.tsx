26dc8d1248ca82b2dcbab72efe358aa5df17e865
Nathan <npieraut@live.fr>
2023-06-05 17:34:25 -0400

    no more props

--- components/_layouts/WebApp.tsx
+++ components/_layouts/WebApp.tsx
@@ -5,19 +5,9 @@ import { Meals } from "../MealsListSide";
 import styles from "../../styles/WebApp.module.css";
 
 interface WebAppProps {
-    likedMeals: any;
-    meals: any;
-    likeMeal: any;
-    unlikeMeal: any;
-    setLikedMeals: any;
-    addMeal: any;
-    removeMealFromList: any;
-    ingredients: any;
-    addIngredient: any;
-    deleteIngredient: any;
 }
 
-const WebApp = ({likedMeals, likeMeal, meals, unlikeMeal, setLikedMeals, addMeal, removeMealFromList}:WebAppProps) => (
+const WebApp = ({}:WebAppProps) => (
     <div className={styles.flexMain}>
         <div style={{ width: '80%', display: 'inline-block'}}>
             <Calendar/>
