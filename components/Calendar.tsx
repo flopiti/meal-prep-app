568ddc7b4da0df7feba864761cf68de325b7b02f
Nathan <npieraut@live.fr>
2023-06-04 20:52:22 -0400

    3 days not 4

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -79,7 +79,7 @@ export default Calendar;
 
 export const getDateStrings = (mainDate: Date) => {
   const dateStrings = [];
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 3; i++) {
     const date = new Date(mainDate.getTime() + i * 24 * 60 * 60 * 1000);
     const dateString = date.toISOString().slice(0, 10);
     dateStrings.push(dateString);
