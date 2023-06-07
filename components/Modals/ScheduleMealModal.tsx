a88b3f631c258e2b7c9db66f5090f95e2b969ed4
Nathan <npieraut@live.fr>
2023-06-07 11:40:44 -0400

    button

--- components/Modals/ScheduleMealModal.tsx
+++ components/Modals/ScheduleMealModal.tsx
@@ -3,7 +3,7 @@ import { useMealContext } from "@/providers/MealContext";
 import { useScheduledMealContext } from "@/providers/ScheduledMealContext";
 import styles from '@/styles/ScheduleMealModal.module.css';
 import Image from 'next/image';
-const ScheduleMealModal = ({closeForm} :any) => {
+const ScheduleMealModal = ({closeForm, day, mealType} :any) => {
     const {meals} = useMealContext();  
     const {scheduleMeal} = useScheduledMealContext();
     const { postScheduledMeal } = useScheduledMeals();
@@ -14,8 +14,24 @@ const ScheduleMealModal = ({closeForm} :any) => {
             meals.map((meal:any) => {
                 return <div className={styles.meal}>
                     <div className={styles.mealName}>{meal.name}</div>
+                    <button onClick={() => {
+                        postScheduledMeal(day, mealType, meal.id).then((res:any) => {
+                            scheduleMeal({
+                                mealName: res.mealName,
+                                date: res.date,
+                                mealType: res.mealType,
+                                id: res.id, 
+                                iconUrl: res.iconUrl,
+                                mealId: meal.id
+                            });
+                            closeForm();
+                        });
+                    }
+                    }
+                    className={styles.mealButton}
+                    >
                     <Image src={meal.iconUrl} alt={''} width={100} height={100}/>
-                
+                    </button>
                 </div>
             }
             )
