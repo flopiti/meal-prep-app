75aaac37bfd7371cd19838eeb68c17f2df81c253
Nathan <npieraut@live.fr>
2023-07-10 11:15:22 -0400

    cleanup

--- components/Day.tsx
+++ components/Day.tsx
@@ -1,38 +1,47 @@
 import styles from '@/styles/Home.module.css';
 import ScheduledMeal from './ScheduledMeal';
 import { useScheduledMealContext } from '@/providers/ScheduledMealContext';
-const Day = ({ day } :any) => {
-    const { scheduledMeals } = useScheduledMealContext();
-    const mealsOfTheDay = scheduledMeals.filter((meal:any) => meal.date === day);
-    let mealTypes = ['breakfast', 'lunch', 'dinner'];
+import { MealType, mealTypes } from '@/types/MealType';
+import { getOrdinalSuffix } from '@/utils/GetOrdinalSuffix';
 
-    day = new Date(day);
-    day.setMinutes(day.getMinutes() + day.getTimezoneOffset());
-    const dayOfWeek = day.toLocaleDateString('en-US', { weekday: 'long' });
-    const dayOfMonth = day.getDate();
+interface DayProps {
+  day: Date;
+}
+
+const Day: React.FC<DayProps> = ({ day }) => {
+  const { scheduledMeals } = useScheduledMealContext();
+  
+  const mealsOfTheDay = scheduledMeals.filter((meal: any) => meal.date === day);
+  
+  const formatDate = (date: Date) => {
+    const adjustedDate = new Date(date);
+    adjustedDate.setMinutes(adjustedDate.getMinutes() + adjustedDate.getTimezoneOffset());
+    
+    const dayOfWeek = adjustedDate.toLocaleDateString('en-US', { weekday: 'long' });
+    const dayOfMonth = adjustedDate.getDate();
     const ordinalSuffix = getOrdinalSuffix(dayOfMonth);
-    const formattedDate = `${dayOfWeek} ${dayOfMonth}${ordinalSuffix}`;
-    return (<div>
-            <div className={styles.date}>
-                {formattedDate}
-            </div>
-            <div className={styles.day}>
-                <ScheduledMeal meal={mealsOfTheDay.find((meal:any) => meal.mealType === mealTypes[0]) ? mealsOfTheDay.find((meal:any) => meal.mealType === mealTypes[0]) : null} mealType={mealTypes[0]} day={day.toISOString().slice(0, 10)} />
-                <ScheduledMeal meal={mealsOfTheDay.find((meal:any) => meal.mealType === mealTypes[1]) ? mealsOfTheDay.find((meal:any) => meal.mealType === mealTypes[1]) : null} mealType={mealTypes[1]} day={day.toISOString().slice(0, 10)} />
-                <ScheduledMeal meal={mealsOfTheDay.find((meal:any) => meal.mealType === mealTypes[2]) ? mealsOfTheDay.find((meal:any) => meal.mealType === mealTypes[2]) : null} mealType={mealTypes[2]} day={day.toISOString().slice(0, 10)}/>
-            </div>
-        </div>
+    
+    return `${dayOfWeek} ${dayOfMonth}${ordinalSuffix}`;
+  }
+  
+  const formattedDate = formatDate(day);
+
+  const renderScheduledMeal = (mealType: MealType) => {
+    const meal = mealsOfTheDay.find((meal: any) => meal.mealType === mealType) || null;
+    return (
+      <ScheduledMeal meal={meal} mealType={mealType} day={new Date(day).toISOString().slice(0, 10)} />
     );
-    };
+  }
+  return (
+    <div>
+      <div className={styles.date}>
+        {formattedDate}
+      </div>
+      <div className={styles.day}>
+        {mealTypes.map(renderScheduledMeal)}
+      </div>
+    </div>
+  );
+};
 
 export default Day;
-
-export function getOrdinalSuffix(day: number): string {
-    if (day > 3 && day < 21) return 'th';
-    switch (day % 10) {
-      case 1:  return "st";
-      case 2:  return "nd";
-      case 3:  return "rd";
-      default: return "th";
-    }
-}
