a8358b938c8ec7c6f1b02e4f290d1ed57c2527b3
Nathan <npieraut@live.fr>
2023-07-10 19:16:06 -0400

    change url

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,5 +1,4 @@
-
-import React, { useEffect, useState } from 'react'
+import {useEffect, useState } from 'react'
 import styles from '@/styles/Home.module.css'
 import Day from '@/components/Day'
 import useSwipe from '@/hooks/useSwipe';
