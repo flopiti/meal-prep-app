72c018de5e702bfa7ec5f8aeb38dc87f601c955c
Nathan <npieraut@live.fr>
2023-07-10 18:43:18 -0400

    props

--- components/_layouts/BaseLayout.tsx
+++ components/_layouts/BaseLayout.tsx
@@ -1,8 +1,11 @@
-import React from "react";
 import styles from "@/styles/Home.module.css";
 import ProfileIcon from "../ProfileIcon/ProfileIcon";
 
-const BaseLayout = ({ children } : any) => {
+interface BaseLayoutProps {
+    children: React.ReactNode;
+}
+
+const BaseLayout = ({ children } : BaseLayoutProps) => {
     return (
             <div className={styles.main}>
                 <ProfileIcon name={""} picture={""} />
