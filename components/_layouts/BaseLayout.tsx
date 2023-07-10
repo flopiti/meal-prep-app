44b9924ed1c1fa0d76aab0576eb8abf9ef67da10
Nathan <npieraut@live.fr>
2023-07-10 10:11:21 -0400

    remove useless

--- components/_layouts/BaseLayout.tsx
+++ components/_layouts/BaseLayout.tsx
@@ -1,10 +1,6 @@
-//make a baseLayout component
-
 import React from "react";
 import styles from "@/styles/Home.module.css";
-import Image from "next/image";
 import ProfileIcon from "../ProfileIcon/ProfileIcon";
-import { motion } from "framer-motion";
 
 const BaseLayout = ({ children } : any) => {
     return (
