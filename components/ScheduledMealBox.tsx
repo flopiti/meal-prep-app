1803dfc17338c49d1f195ef5a3db15ef59013dad
Nathan <npieraut@live.fr>
2023-07-10 16:36:12 -0400

    scheduledMealBox

--- components/ScheduledMealBox.tsx
+++ components/ScheduledMealBox.tsx
@@ -18,15 +18,11 @@ const ScheduledMealBox = ({mealId, mealName, iconUrl, deleteMeal,scheduledMealId
     };
 
     const handleImageLoad = () => {
-        setIsImageLoaded(true); // This will set isImageLoaded to true when the image loads
+        setIsImageLoaded(true);
     };
     
     return (
-        <div className={styles.containerMeal} 
-        key={mealId}
-        draggable
-        onDragStart={()=>handleDragStart(event, mealId)}
-        >    
+        <div className={styles.containerMeal} key={mealId} draggable onDragStart={()=>handleDragStart(event, mealId)}>    
             {!isImageLoaded && <Skeleton width={'100%'} height={100}
             />}
 
@@ -34,27 +30,25 @@ const ScheduledMealBox = ({mealId, mealName, iconUrl, deleteMeal,scheduledMealId
             className={styles.mealChoice}
             animate={{  scale: [0.25, 1] }}
             transition={{ duration: .5, type : "spring", stiffness: 200}}
-            style={{ visibility: isImageLoaded ? 'visible' : 'hidden' }} // Hide until image is loaded
+            style={{ visibility: isImageLoaded ? 'visible' : 'hidden' }} 
             >
-
-                <motion.div
-                    className={styles.mealIcon}
-                    initial={{ y: 150 }}
-                    animate={{ y: [150,5], rotate: [0, 360] }}
-                    transition={{delay:.25, duration: .5, stiffness: 100}}
-                >
-                    {iconUrl && (
-                        <Image 
-                          src={iconUrl} 
-                          alt="food" 
-                          width={100} 
-                          height={100} 
-                          onLoad={handleImageLoad}
-                          style={{ visibility: isImageLoaded ? 'visible' : 'hidden' }} // Change visibility when image is loaded
-                        />
-                    )}
-                </motion.div>
-
+            <motion.div
+                className={styles.mealIcon}
+                initial={{ y: 150 }}
+                animate={{ y: [150,5], rotate: [0, 360] }}
+                transition={{delay:.25, duration: .5, stiffness: 100}}
+            >
+            {iconUrl && (
+                <Image 
+                    src={iconUrl} 
+                    alt="food" 
+                    width={100} 
+                    height={100} 
+                    onLoad={handleImageLoad}
+                    style={{ visibility: isImageLoaded ? 'visible' : 'hidden' }}
+                />
+            )}
+            </motion.div>
                 <div className={styles.mealName}>
                     {mealName }
                 </div>
